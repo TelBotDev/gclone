30c00616ffa2638114086921d24691841ac2b5db
She <pter.zh@gmail.com>
2021-03-24 21:55:17 +0800

    err.Error() rather than err.msg

--- backend/drive/drive.go
+++ backend/drive/drive.go
@@ -640,7 +640,7 @@ func (f *Fs) shouldRetry(err error) (bool, error) {
 
 	// added by me
 	default:
-        reason := err.msg
+        reason := err.Error()
         if reason == "Max transfer limit reached as set by --max-transfer" {
             // do something
             if(f.opt.ServiceAccountFilePath != ""){
